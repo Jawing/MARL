43175a5da854a74f1457bf871f49a6b38575fa08
Jawing <wjz199@gmail.com>
2023-04-17 02:38:40 -0400

    PPO agent

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ conda activate rl-env
 conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
 
 # Install other dependencies
-pip install matplotlib tqdm jupyter ipywidgets scikit-learn pettingzoo==1.22.3 pettingzoo[atari] supersuit==3.6.0
+pip install matplotlib tqdm jupyter ipywidgets scikit-learn pettingzoo==1.22.4 pettingzoo[atari] supersuit==3.7.2
 
 # Set up for the Atari environments and install the ROMs (Linux)
 pip install "autorom[accept-rom-license]"
@@ -17,3 +17,9 @@ AutoROM
 
 # Record videos of the environments
 pip install opencv-python
+
+# #(optional) Install tianshou and other dependencies
+# pip install tianshou packaging
+
+# #(optional) Install stable-baselines3 and other dependencies
+# pip install stable-baselines3[extra] pyglet
